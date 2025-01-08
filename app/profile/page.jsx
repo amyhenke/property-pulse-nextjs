9e2594b8e124b2bfece35738ac05905d87af7e10
Amy Henke <amyleehenke1@gmail.com>
2025-01-08 21:06:18 +0000

    import error fix

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,13 +1,12 @@
-import Image from 'next/image';
-import connectDB from '@/config/database';
-import Property from '../models/Property';
-import { getSessionUser } from '@/utils/getSessionUser';
+import Image from "next/image"
+import connectDB from "@/config/database"
+import Property from "../models/Property"
+import { getSessionUser } from "@/utils/getSessionUser"
 import profileDefault from "@/assets/images/profile.png"
-import ProfileProperties from '@/components/profileProperties';
-import { convertToSerializeableObject } from '@/utils/convertToObject';
+import ProfileProperties from "@/components/ProfileProperties"
+import { convertToSerializeableObject } from "@/utils/convertToObject"
 
 const ProfilePage = async () => {
-
     await connectDB()
 
     const sessionUser = await getSessionUser()
@@ -15,29 +14,21 @@ const ProfilePage = async () => {
     const { userId } = sessionUser
 
     if (!userId) {
-        throw new Error('User ID is required')
+        throw new Error("User ID is required")
     }
 
-    const propertiesDocs = await Property.find({owner: userId}).lean()
+    const propertiesDocs = await Property.find({ owner: userId }).lean()
     const properties = propertiesDocs.map(convertToSerializeableObject)
 
-    return ( 
+    return (
         <section className="bg-blue-50">
             <div className="container m-auto py-24">
-                <div
-                className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
-                >
+                <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
                     <h1 className="text-3xl font-bold mb-4">Your Profile</h1>
                     <div className="flex flex-col md:flex-row">
                         <div className="md:w-1/4 mx-20 mt-10">
                             <div className="mb-4">
-                                <Image
-                                    className="h-32 w-32 md:h-48 md:w-48 rounded-full mx-auto md:mx-0"
-                                    src={sessionUser.user.image || profileDefault}
-                                    width={200}
-                                    height={200}
-                                    alt="User"
-                                />
+                                <Image className="h-32 w-32 md:h-48 md:w-48 rounded-full mx-auto md:mx-0" src={sessionUser.user.image || profileDefault} width={200} height={200} alt="User" />
                             </div>
 
                             <h2 className="text-2xl mb-4">
@@ -56,7 +47,7 @@ const ProfilePage = async () => {
                 </div>
             </div>
         </section>
-     );
+    )
 }
- 
-export default ProfilePage;
+
+export default ProfilePage
