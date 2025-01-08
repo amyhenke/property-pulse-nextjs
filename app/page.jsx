4bf9e4f5b7280647ddab8544985ee11d8962a09c
Amy Henke <amyleehenke1@gmail.com>
2025-01-08 20:06:31 +0000

    image lightbox and featured properties

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,17 @@
-import Hero from "@/components/Hero";
-import InfoBoxes from "@/components/InfoBoxes";
-import HomeProperties from "@/components/HomeProperties";
+import Hero from "@/components/Hero"
+import InfoBoxes from "@/components/InfoBoxes"
+import HomeProperties from "@/components/HomeProperties"
+import FeaturedProperties from "@/components/FeaturedProperties"
 
 const HomePage = () => {
-    return ( <div>
-        <Hero />
-        <InfoBoxes />
-        <HomeProperties />
-    </div> );
+    return (
+        <div>
+            <Hero />
+            <InfoBoxes />
+            <FeaturedProperties />
+            <HomeProperties />
+        </div>
+    )
 }
- 
-export default HomePage;
+
+export default HomePage
